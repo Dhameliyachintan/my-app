32b108ea0d6aefa24729be1bba4d737e30007dd4
Dhameliyachintan <chintandhameliya07@gmail.com>
2022-04-04 10:16:55 +0530

    31-03

--- src/container/Array.js
+++ src/container/Array.js
@@ -1,9 +1,7 @@
 import React from 'react';
 
-function Array(props) {
-
     // ------------------Array---------------------//
-function App() {
+function Array() {
 
    let arr1 =[10,20,30];
       let arr2 =[10,20];
@@ -26,34 +24,6 @@ function App() {
       console.log(arr6[0]);
       console.log(fname);
       // console.log(lname);
-}
-
-// ------------Object-----------------//
-
-function App(props) {
-  let obj1 = {fname:'Amit',age:18};
-  let obj2 = {lname:'parth',age:20};
-
-  //copy
-  let obj3 = {...obj1};
-  console.log(obj3);
-
-  //concate
-  let obj4 = {...obj1,...obj2}; 
-  // let obj5 = {...obj1,...obj2};
-  console.log(obj4);
-
-  //concate
-  let obj5 = {fname:'Amit',age:18,city:'surat'};
-  let obj6 = {fname:'red',age:18,city:'surat'};
-  let obj7 = {fname:'red',age:18,city:'surat'};
-  console.log(obj4);
-}
-    return (
-        <div>
-            
-        </div>
-    );
 }
 
 export default Array;
